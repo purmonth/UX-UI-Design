75d0d09f392ce77e94c4b67147fd62377bd083b4
purmonth <purmonth@gmail.com>
2020-04-06 22:50:55 +0800

    Add about page border

--- public_html/FinalProject/About.js
+++ public_html/FinalProject/About.js
@@ -1,6 +1,6 @@
 $(document).ready(function(){
     $(".header_block").click(()=>{
-        $(".group").removeClass("hidden");
+        $(".group,.container_border").removeClass("hidden");
         $(".container_introduce").addClass("hidden fadeIn");
     })
     $("#group1").click(() => {
@@ -11,28 +11,28 @@ $(document).ready(function(){
         $(".introduce_title").empty().append("Ting-Yi<br>WU<br>Designer");
     });
     $("#group2").click(() => {
-        $(".group").addClass("hidden");
+        $(".group,.container_border").addClass("hidden");
         $(".container_introduce").removeClass("hidden");
         $(".introduce_charc").attr('id','charc2');
         $(".introduce_header").empty().append("ELECTRICAL ENGINEERING");
         $(".introduce_title").empty().append("Hung-Yueh<br>Lin<br>Engineer");
     });
     $("#group3").click(() => {
-        $(".group").addClass("hidden");
+        $(".group,.container_border").addClass("hidden");
         $(".container_introduce").removeClass("hidden");
         $(".introduce_charc").attr('id','charc3');
         $(".introduce_header").empty().append("ENGINEERING SCIENCE");
         $(".introduce_title").empty().append("<br>Allision<br>Engineer");
     });
     $("#group4").click(() => {
-        $(".group").addClass("hidden");
+        $(".group,.container_border").addClass("hidden");
         $(".container_introduce").removeClass("hidden");
         $(".introduce_charc").attr('id','charc4');
         $(".introduce_header").empty().append("ELECTRICAL ENGINEERING");
         $(".introduce_title").empty().append("<br>JiaWei<br>Engineer");
     });
     $("#group5").click(() => {
-        $(".group").addClass("hidden");
+        $(".group,.container_border").addClass("hidden");
         $(".container_introduce").removeClass("hidden");
         $(".introduce_charc").attr('id','charc5');
         $(".introduce_header").empty().append("ELECTRICAL ENGINEERING");
