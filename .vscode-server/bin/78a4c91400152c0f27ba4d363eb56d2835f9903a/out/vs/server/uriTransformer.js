6a3ede69993869509e6eba4b30d209f49ceef953
purmonth <purmonth@gmail.com>
2020-05-03 10:57:04 +0800

    Merge page

--- .vscode-server/bin/78a4c91400152c0f27ba4d363eb56d2835f9903a/out/vs/server/uriTransformer.js
+++
@@ -1,5 +0,0 @@
-/*---------------------------------------------------------------------------------------------
- *  Copyright (c) Microsoft Corporation. All rights reserved.
- *--------------------------------------------------------------------------------------------*/
-module.exports=function(e){return{transformIncoming:e=>"vscode-remote"===e.scheme?{scheme:"file",path:e.path}:"file"===e.scheme?{scheme:"vscode-local",path:e.path}:e,transformOutgoing:o=>"file"===o.scheme?{scheme:"vscode-remote",authority:e,path:o.path}:"vscode-local"===o.scheme?{scheme:"file",path:o.path}:o,transformOutgoingScheme:e=>"file"===e?"vscode-remote":"vscode-local"===e?"file":e}};
-//# sourceMappingURL=https://ticino.blob.core.windows.net/sourcemaps/78a4c91400152c0f27ba4d363eb56d2835f9903a/core/vs/server/uriTransformer.js.map
--- .vscode-server/bin/fe22a9645b44368865c0ba92e2fb881ff1afce94/out/vs/platform/files/node/watcher/nsfw/watcherApp.nls.js
+++
@@ -1,5 +0,0 @@
-/*!--------------------------------------------------------
- * Copyright (C) Microsoft Corporation. All rights reserved.
- *--------------------------------------------------------*/
-define("vs/platform/files/node/watcher/nsfw/watcherApp.nls",{"vs/base/node/processes":["Can't execute a shell command on a UNC drive."],"vs/platform/files/common/files":["Unknown Error"]});
-//# sourceMappingURL=https://ticino.blob.core.windows.net/sourcemaps/fe22a9645b44368865c0ba92e2fb881ff1afce94/core/vs/platform/files/node/watcher/nsfw/watcherApp.nls.js.map
