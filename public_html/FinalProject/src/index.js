531674814b71cd0e4241abfe2f9380e5892bbb2a
purmonth <purmonth@gmail.com>
2020-05-06 16:03:54 +0800

    Fix bugs

--- public_html/FinalProject/src/index.js
+++ public_html/FinalProject/src/index.js
@@ -1,16 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import HomePage from './page/HomePage.jsx'
-import LogInPage from './page/LogInPage.jsx'
+import LoginPage from './page/LoginPage.jsx'
 import SignUpPage from './page/SignupPage.jsx'
 
 class Root extends React.Component{
     render(){
         return(
             <div>
-                <HomePage />
-                <LogInPage />
-                <SignUpPage />
+            <HomePage />
+            <LoginPage />
+            <SignUpPage />
             </div>
         )
     }
